1a72665007990840beb560f5dd926f76317fabd4
maurisan4011 <maurisan4011@gmail.com>
2019-11-28 02:10:56 +0000

    23102019

--- web_personal_3/web_personal_3/urls.py
+++ web_personal_3/web_personal_3/urls.py
@@ -20,7 +20,7 @@ from portfolio import views as portfolio_views
 from django.conf import settings
 
 urlpatterns = [
-    path('home/', core_views.home, name='home'),
+    path('', core_views.home, name='home'),
     path('about/', core_views.about, name='about'),
     path('portfolio/', portfolio_views.portfolio, name='portfolio'),
     path('contact/', core_views.contact, name='contact'),
@@ -29,7 +29,7 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    
+
     from django.conf.urls.static import static
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
